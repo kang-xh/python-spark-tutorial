24123193c7c2950405b296d812fc7e1bfbb108f1
kang-xh <kang_xh@hotmail.com>
2019-12-25 10:03:05 +0800

    tst

--- test/rdd.py
+++ test/rdd.py
@@ -1,4 +1,5 @@
 import sys, datetime
+
 sys.path.insert(0,'.')
 from commons.Utils import Utils
 from pyspark import SparkContext, SparkConf
@@ -7,8 +8,8 @@ if __name__ == "__main__":
     sparkConfig = SparkConf().setAppName('airports').setMaster('local[*]')
     sparkContext = SparkContext(conf = sparkConfig)
 
-    airports_RDD = sparkContext.textFile('out/airports_exec_2019-12-25-07-54.text')
+    airports_RDD = sparkContext.textFile('out/airport_exec_2019-12-25-07-54.text/part-*')
 
     count = airports_RDD.count()
 
-    print(count)
+    print('output part-000* contains {} lines'.format(count))
